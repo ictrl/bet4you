afb9f78d69a2ca80f5a33ece94634dd41f307077
Samrat Jaiswal <saurav.k@harnstech.com>
2022-03-29 23:31:54 +0530

    fix mysql

--- utils/MySql/connect.js
+++ utils/MySql/connect.js
@@ -16,13 +16,27 @@ connection.connect(function (err) {
 
 const connect = async (match_id) => {
   const query = `Select * from odd_bet where match_id=${match_id} and src='api'`;
+  const res = await performQuery(connection, query);
+  return res[0];
 
-  connection.query(query, function (error, results, fields) {
-    if (error) throw error;
-    console.log("The solution is: ", results[0].match_id);
-  });
-
-  // connection.end();
+  connection.end();
 };
+
+/* promisified query method, suitable for await */
+function performQuery(connection, query) {
+  try {
+    return new Promise((resolve, reject) => {
+      connection.query(query, function (error, result) {
+        if (error) {
+          // return reject(error);
+          resolve(error);
+        }
+        resolve(result);
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = connect;
