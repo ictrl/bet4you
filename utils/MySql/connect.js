6d9ca9f39dc242488c50eeb202097b2450e058a5
Samrat Jaiswal <saurav.k@harnstech.com>
2022-03-29 23:21:24 +0530

    fix mysql

--- utils/MySql/connect.js
+++ utils/MySql/connect.js
@@ -1,47 +1,28 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: process.env.host,
+  user: process.env.user,
+  database: process.env.database,
+  password: process.env.password,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 const connect = async (match_id) => {
-  try {
-    //connect to mysql db
-    const connection = mysql.createConnection({
-      host: process.env.host,
-      user: process.env.user,
-      database: process.env.database,
-      password: process.env.password,
-    });
+  const query = `Select * from odd_bet where match_id=${match_id} and src='api'`;
 
-    connection.connect(function (err) {
-      if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-      }
-    });
-    // query = `SELECT * FROM session_bet where result_status='pending' and src='manual' and is_active='1'`;
-    query = `Select * from odd_bet where match_id=${match_id} and src='api'`;
-    const res = await performQuery(connection, query);
-    return res[0] || {};
+  connection.query(query, function (error, results, fields) {
+    if (error) throw error;
+    console.log("The solution is: ", results[0].match_id);
+  });
 
-    connection.end();
-  } catch (error) {
-    console.error({ error: error.message, table });
-  }
+  // connection.end();
 };
-
-/* promisified query method, suitable for await */
-function performQuery(connection, query) {
-  try {
-    return new Promise((resolve, reject) => {
-      connection.query(query, function (error, result) {
-        if (error) {
-          // return reject(error);
-          resolve(error);
-        }
-        resolve(result);
-      });
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 module.exports = connect;
