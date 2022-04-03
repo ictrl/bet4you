1e179b1631153ced41139fa1d72ab4fa183c1369
Samrat Jaiswal <saurav.k@harnstech.com>
2022-04-03 06:06:23 +0530

    upload load script

--- load.js
+++ load.js
@@ -1,12 +1,50 @@
 const WebSocket = require("ws");
-const url = "ws://localhost:5074";
-
-for (let i = 0; i < 10000; i++) {
+const url1 = "ws://3.111.105.44:3075"; //session
+const count = 1000;
+for (let i = 0; i < count; i++) {
   console.log({ i });
-  const connection = new WebSocket(url);
+  const connection = new WebSocket(url1);
 
   connection.onopen = () => {
-    connection.send("31323015 manual");
+    connection.send("31342464 api");
+  };
+
+  connection.onerror = (error) => {
+    console.log(`WebSocket error: ${error}`);
+  };
+
+  connection.onmessage = (e) => {
+    console.log(e.data);
+  };
+}
+
+const url2 = "ws://3.111.105.44:9069"; //odds
+
+for (let i = 0; i < count; i++) {
+  console.log({ i });
+  const connection = new WebSocket(url2);
+
+  connection.onopen = () => {
+    connection.send("31342464");
+  };
+
+  connection.onerror = (error) => {
+    console.log(`WebSocket error: ${error}`);
+  };
+
+  connection.onmessage = (e) => {
+    console.log(e.data);
+  };
+}
+
+const url3 = "ws://3.111.105.44:5074"; //scorecard
+
+for (let i = 0; i < count; i++) {
+  console.log({ i });
+  const connection = new WebSocket(url3);
+
+  connection.onopen = () => {
+    connection.send("31342464 manual");
   };
 
   connection.onerror = (error) => {
