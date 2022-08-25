ff77182decc17ab6797c7700ae67af1b21f41bd0
root <root@ip-172-31-8-243.ap-south-1.compute.internal>
2022-08-25 20:43:09 +0530

    server changes

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 module.exports = {
   ODDS: {
     port: 9069,
-    frequency: 2000,
+    frequency: 1200,
   },
   SCORECARD: {
     port: 5074,
-    frequency: 2000,
+    frequency: 2500,
   },
   SESSION: {
     port: 3075,
-    frequency: 2200,
+    frequency: 1300,
   },
   CASINO: {
     port: 7000,
-    frequency: 2000,
+    frequency: 2200,
   },
-  ORIGINS: ["http://bet4you.in"],
+	ORIGINS: ["http://bet4you.in","http://wbt999.co","http://bsf999.in","http://bsf0007.com","http://3.111.105.44"],
 };
